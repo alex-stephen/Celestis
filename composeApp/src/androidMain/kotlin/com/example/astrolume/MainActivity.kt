9da9b8c6517dc8d9723af285bf9aaeab60916122
Alex Stephen <alemccsteYT@gmail.com>
2026-03-05 00:47:11 +0100

    chore: fixed build config, gradle. Getting data from server. TODO: Fix buildkonfig

--- composeApp/src/androidMain/kotlin/com/example/astrolume/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/astrolume/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.example.astrolume.android
+package com.example.astrolume
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.astrolume.database.AppDatabase
 import com.example.astrolume.database.DatabaseDriverFactory
 
